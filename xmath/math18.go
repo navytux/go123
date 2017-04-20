b1893d270e87039c89ce1108b964273c1bc5432b
Kirill Smelkov <kirr@nexedi.com>
2017-04-20 16:05:45 +0300

    xmath: Addon math routines
    So far only one of them: CeilPow2 to return min(y) >= x: y = 2^i - i.e.
    next power of two >= x. This is handy to have in reallocation routines
    to allocate buffers from 2^i classes.
    
    Two implementations:
    
    - fast for go19
    - slower fallback for go18

---
+++ xmath/math18.go
@@ -0,0 +1,46 @@
+// Copyright (C) 2017  Nexedi SA and Contributors.
+//                     Kirill Smelkov <kirr@nexedi.com>
+//
+// This program is free software: you can Use, Study, Modify and Redistribute
+// it under the terms of the GNU General Public License version 3, or (at your
+// option) any later version, as published by the Free Software Foundation.
+//
+// You can also Link and Combine this program with other software covered by
+// the terms of any of the Open Source Initiative approved licenses and Convey
+// the resulting work. Corresponding source of such a combination shall include
+// the source code for all other software used.
+//
+// This program is distributed WITHOUT ANY WARRANTY; without even the implied
+// warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+//
+// See COPYING file for full licensing terms.
+
+// +build !go1.9
+
+package xmath
+
+// CeilPow2 returns minimal y >= x, such that y = 2^i
+func CeilPow2(x uint64) uint64 {
+	if x == 0 {
+		return x
+	}
+
+	l := uint(0)
+	h := uint(63)
+	for l < h {
+		i := (l + h) / 2
+		y := uint64(1) << i
+
+		switch {
+		case y < x:
+			l = i + 1
+		case y > x:
+			h = i
+		default:
+			// y == x
+			return y
+		}
+	}
+
+	return 1 << h
+}
