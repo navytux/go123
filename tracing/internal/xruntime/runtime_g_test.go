cc88752ef153094d7fa91e8804f4897cdba6d943
Kirill Smelkov <kirr@nexedi.com>
2026-02-13 11:29:24 +0300

    tracing/runtime: Add test for getg + retrieved g struct
    Since c0f14991 (tracing: Part 3 - Silence race-detector about
    probe.Detach) we are extracting the definition of `struct g` from Go
    runtime and also have custom assembly getg function to retrieve g
    pointer of current goroutine. It used to work in manual tests. However
    there was no automatic testing that it was correct.
    
    Add simple test that verifies that getg returns sensible result via
    checking returned g's fields against public information about that
    goroutine that we can retrieve via another ways without hacks. That
    currently comes with g.goid, retrieved via runtime.Stack, and g.startpc,
    because if we start a goroutine ourselves we know what we start.
    
    And added test shows that though getg and g.goid seem to be correct, we
    have a problem with g.startpc:
    
        --- FAIL: TestGetG (0.01s)
            runtime_g_test.go:30: XFAIL: currently fails wrt g.startpc
            runtime_g_test.go:49: goroutine 7: have goid=7  startpc=0x0 ();  want goid=7  startpc=0x53ae40 (lab.nexedi.com/kirr/go123/tracing/internal/xruntime.ginfo)
            runtime_g_test.go:49: goroutine 24: have goid=24  startpc=0x0 ();  want goid=24  startpc=0x53ae40 (lab.nexedi.com/kirr/go123/tracing/internal/xruntime.ginfo)
            runtime_g_test.go:49: goroutine 9: have goid=9  startpc=0x0 ();  want goid=9  startpc=0x53ae40 (lab.nexedi.com/kirr/go123/tracing/internal/xruntime.ginfo)
            runtime_g_test.go:49: goroutine 10: have goid=10  startpc=0x0 ();  want goid=10  startpc=0x53ae40 (lab.nexedi.com/kirr/go123/tracing/internal/xruntime.ginfo)
            runtime_g_test.go:49: goroutine 18: have goid=18  startpc=0x0 ();  want goid=18  startpc=0x53ae40 (lab.nexedi.com/kirr/go123/tracing/internal/xruntime.ginfo)
            runtime_g_test.go:49: goroutine 8: have goid=8  startpc=0x0 ();  want goid=8  startpc=0x53ae40 (lab.nexedi.com/kirr/go123/tracing/internal/xruntime.ginfo)
            runtime_g_test.go:49: goroutine 19: have goid=19  startpc=0x0 ();  want goid=19  startpc=0x53ae40 (lab.nexedi.com/kirr/go123/tracing/internal/xruntime.ginfo)
            runtime_g_test.go:49: goroutine 21: have goid=21  startpc=0x0 ();  want goid=21  startpc=0x53ae40 (lab.nexedi.com/kirr/go123/tracing/internal/xruntime.ginfo)
            runtime_g_test.go:49: goroutine 22: have goid=22  startpc=0x0 ();  want goid=22  startpc=0x53ae40 (lab.nexedi.com/kirr/go123/tracing/internal/xruntime.ginfo)
    
    We will fix this problem in the next patch.

---
+++ tracing/internal/xruntime/runtime_g_test.go
@@ -0,0 +1,85 @@
+// Copyright (C) 2026  Nexedi SA and Contributors.
+//                     Kirill Smelkov <kirr@nexedi.com>
+//
+// This program is free software: you can Use, Study, Modify and Redistribute
+// it under the terms of the GNU General Public License version 3, or (at your
+// option) any later version, as published by the Free Software Foundation.
+//
+// You can also Link and Combine this program with other software covered by
+// the terms of any of the Free Software licenses or any of the Open Source
+// Initiative approved licenses and Convey the resulting work. Corresponding
+// source of such a combination shall include the source code for all other
+// software used.
+//
+// This program is distributed WITHOUT ANY WARRANTY; without even the implied
+// warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+//
+// See COPYING file for full licensing terms.
+// See https://www.nexedi.com/licensing for rationale and options.
+
+package xruntime
+
+import (
+	"fmt"
+	"reflect"
+	"runtime"
+	"testing"
+)
+
+func TestGetG(t *testing.T) {
+	t.Log("XFAIL: currently fails wrt g.startpc")
+
+	// verify that getg returns reasonable result by checking g.goid and g.startpc
+	n := 1000
+	for i := 0; i < n; i++ {
+		// NOTE we are spawning ginfo without arguments so that startpc goes
+		//      directly to ginfo instead of to closure
+		go ginfo()
+	}
+
+	ginfo_pc   := reflect.ValueOf(ginfo).Pointer()
+	ginfo_func := runtime.FuncForPC(ginfo_pc)
+	for i := 0; i < n; i++ {
+		tg := <-tginfoq
+		if tg.err != nil {
+			t.Error(tg.err)
+		}
+		if !(tg.g_goid == tg.goid && tg.g_startpc == ginfo_pc) {
+			g_start_func := runtime.FuncForPC(tg.g_startpc)
+			t.Errorf("goroutine %d: have goid=%d  startpc=0x%x (%s);  want goid=%d  startpc=0x%x (%s)",
+				tg.goid, tg.g_goid, tg.g_startpc, g_start_func.Name(), tg.goid, ginfo_pc, ginfo_func.Name())
+		}
+	}
+}
+
+var tginfoq = make(chan tGInfo)
+type tGInfo struct {
+	g_goid    uint64  // g.goid
+	g_startpc uintptr // g.startpc
+	goid      uint64  // goid retrieved via runtime.Stack
+	err       error
+}
+
+//go:noinline
+func ginfo() {
+	var tg tGInfo
+
+	// retrieve g info
+	g := getg()
+	tg.g_goid    = uint64(g.goid)
+	tg.g_startpc = g.startpc
+
+	// retrieve goid printed by runtime.Stack
+	stkb := make([]byte, 1024)
+	n := runtime.Stack(stkb, false)
+	stk := string(stkb[:n])
+
+	// goroutine <goid> [running]:
+	// ...
+	n, err := fmt.Sscanf(stk, "goroutine %d ", &tg.goid)
+	if !(n == 1 && err == nil) {
+		tg.err = fmt.Errorf("cannot retrieve goid from traceback\n%s", stk)
+	}
+
+	tginfoq <- tg
+}
